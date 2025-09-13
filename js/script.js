60a386562e7a0ce6f1719e0109769c7ddb5ae238
Willian Eliseu da Silva <w.eliseu@gmail.com>
2025-09-13 15:06:46 -0300

    added extra credit content. color variations.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,55 @@
 const container = document.getElementById('container');
 const gridSize = [16];
+const colors = [
+    "#E0B0DD",
+    "#2C3C9A",
+    "#F589B9",
+    "#8159E1",
+    "#A3C828",
+    "#5E9ACD",
+    "#14479E",
+    "#DB5053",
+    "#D91694",
+    "#FC2C23"
+];
+const count = {
+    value: 0,
+    colored: true
+};
 
 window.addEventListener('DOMContentLoaded', () => {
     createItems(gridSize[0]);
 })
 
-function createItems(size){
-    for(let i = 0; i < size; i++){
-        
+function changeColor() {
+    count.colored = !count.colored;
+}
+
+function createItems(size) {
+    for (let i = 0; i < size; i++) {
+
         const row = document.createElement('div');
         row.style.height = `calc(100% / ${size})`;
         row.style.width = '100%';
         row.classList.add('row');
         container.appendChild(row);
 
-        for(let j = 0; j < size; j++){
+        for (let j = 0; j < size; j++) {
             const item = document.createElement('div');
-            item.style.width = `calc(100% / ${size})`;            
+            item.style.width = `calc(100% / ${size})`;
             item.classList.add('item');
             row.appendChild(item);
 
             item.addEventListener('mouseover', () => {
-                item.classList.add('blacked');
+                if (count.colored) {
+                    item.style.backgroundColor = colors[count.value];
+                    count.value++;
+                    if (count.value === colors.length) {
+                        count.value = 0;
+                    }
+                } else {
+                    item.classList.add('blacked');
+                }
             });
         }
     }
@@ -29,25 +57,25 @@ function createItems(size){
     writeBoardHeader(size);
 }
 
-function resetBoard(){
+function resetBoard() {
     container.innerHTML = '';
     createItems(gridSize[0]);
 }
 
-function changeGridSize(){
+function changeGridSize() {
     const newGridSize = prompt('Enter the new grid size. Minimum 16 and maximum 100.');
-    if(newGridSize >= 16 && newGridSize <= 100){
+    if (newGridSize >= 16 && newGridSize <= 100) {
         container.innerHTML = '';
         createItems(newGridSize);
-    }else if(newGridSize == null){
+    } else if (newGridSize == null) {
         resetBoard();
-    }else{
+    } else {
         alert('Invalid Size');
         changeGridSize();
     }
 }
 
-function writeBoardHeader(size){
+function writeBoardHeader(size) {
     let gridSizeElement = document.getElementById('grid-size');
     gridSizeElement.innerText = `${size} x ${size}`;
     gridSizeElement.style.textAlign = 'center';
